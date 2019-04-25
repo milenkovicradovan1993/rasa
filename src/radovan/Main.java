60d06ade02c135ade74f9e023fba8004faf81130
Radovan <Radovan1993@live.com>
2019-04-25 20:43:22 +0200

    First project

---
+++ src/radovan/Main.java
@@ -0,0 +1,18 @@
+package radovan;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int a, o;
+		System.out.println("Unesite duzinu stranice");
+		a = sc.nextInt();
+		o = 4*a;
+		System.out.println("Obim kvadrata iznosi: " + o);
+
+	}
+
+}
